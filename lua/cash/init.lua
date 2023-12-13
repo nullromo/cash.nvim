a9b539689ceb538e7c54c11b8411a8bebe7db7e2
Kyle Kovacs <kylerobertkovacs@gmail.com>
2023-12-13 08:21:51 -0800

    primitive plugin skeleton for seeing how to configure plugins with Lazy

--- lua/cash/init.lua
+++ lua/cash/init.lua
@@ -1,13 +1,34 @@
+-- create module object for export
 local CashModule = {}
 
+-- main setup function for Cash.nvim
 function CashModule.setup(options)
+    -- make sure options is not nil
+    options = options or {}
+
+    -- set default message if one is not provided
+    options.message = options.message or 'hello'
+
+    -- validate options
+    for key, value in pairs(options) do
+        if key == 'message' then
+            if type(value) ~= 'string' then
+                error('options.message must be a string for Cash.nvim')
+            end
+        else
+            -- error!
+            error('"' .. key .. '" is not a valid option for Cash.nvim')
+        end
+    end
+
     vim.api.nvim_create_user_command(
         'TimeToTest',
         function()
-            vim.notify('cool')
+            vim.notify(options.message)
         end,
         { bang = true }
     )
 end
 
+-- export module
 return CashModule
